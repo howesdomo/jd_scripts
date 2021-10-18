4d544e4d2093b907fc1b8a5eefe7c105f82485c9
HowesDOMO <164572429@qq.com>
2021-10-18 12:15:09 +0800

    1 新增自动执行脚本 main.ts 2 模块化 东东农场 3 模块化 京豆 4 优化 东东农场 重复获取用户

---
+++ main.ts
@@ -0,0 +1,55 @@
+// import cfd from './jd_cfd_module'
+// import jxmc from './jd_jxmc_module'
+// import jxgc from './jd_jxgc_module'
+// import beanSign from './jd_bean_sign_module'
+
+import { getDate } from "date-fns";
+
+var log4js = require("log4js")
+var log4js_config = require("./log4js.json")
+log4js.configure(log4js_config);
+
+var logFile = log4js.getLogger('log_file');
+
+function myLog (msg, count) {  
+  var m = msg + " - 第 " + count + " 次执行";
+
+  console.log(m);
+  logFile.info(m);
+}
+
+let countDaily = 1;
+let countMinutes = 1;
+
+var jd_bean_home_module= require('./jd_bean_home_module')
+var jd_fruit_module= require('./jd_fruit_module')
+
+// 已测试
+myLog("一天一运行 - 京豆", countDaily++);
+jd_bean_home_module.run();
+
+myLog("一小时一运行 - 东东农场", countMinutes++);
+jd_fruit_module.run();
+
+// 未测试
+//cfd.run();
+//jxmc.run();
+//jxgc.run();
+//beanSign.run();
+
+setInterval(async function() {
+    myLog("一天一运行 - 京豆", countDaily);
+    await jd_bean_home_module.run();
+    countDaily++;
+  },
+  1000 * 60 * 60 * 24 // 每天执行一次
+);
+
+setInterval(async function()
+  {
+    myLog("一小时一运行 - 东东农场", countMinutes);
+    await jd_fruit_module.run();
+    countMinutes++;
+  },
+  1000 * 60 * 60 // 每 60 分钟执行一次
+);
