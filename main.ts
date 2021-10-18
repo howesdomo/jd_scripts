31d6d996eaa999e6e4ebbcd4f490e3b2b5d43c92
HowesDOMO <164572429@qq.com>
2021-10-18 16:31:56 +0800

    1 京喜牧场模块化 2 新增 main_test.ts 测试定时执行 3 main.ts 加入 京喜牧场脚本

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
+import jd_jxmc_module from './jd_jxmc_module'
 // import cfd from './jd_cfd_module'
-// import jxmc from './jd_jxmc_module'
 // import jxgc from './jd_jxgc_module'
 // import beanSign from './jd_bean_sign_module'
 
@@ -18,18 +18,24 @@ function myLog (msg, count) {
   logFile.info(m);
 }
 
-let countDaily = 1;
-let countMinutes = 1;
+let countDaily = 0;
+let countMinutes = 0;
 
 var jd_bean_home_module= require('./jd_bean_home_module')
 var jd_fruit_module= require('./jd_fruit_module')
 
 // 已测试
-myLog("一天一运行 - 京豆", countDaily++);
+// 每天执行一次
+countDaily++;
+myLog("每 1 天运行 - 京豆", countDaily);
 jd_bean_home_module.run();
 
-myLog("一小时一运行 - 东东农场", countMinutes++);
+// 每 x 分钟执行一次
+countMinutes++;
+myLog("每 60 分钟运行 - 东东农场", countMinutes);
 jd_fruit_module.run();
+myLog("每 60 分钟运行 - 京喜牧场", countMinutes);
+jd_jxmc_module.run();
 
 // 未测试
 //cfd.run();
@@ -37,19 +43,26 @@ jd_fruit_module.run();
 //jxgc.run();
 //beanSign.run();
 
-setInterval(async function() {
-    myLog("一天一运行 - 京豆", countDaily);
+setInterval
+(
+  async function() 
+  {
+    countDaily++;  
+    myLog("每 1 天运行 - 京豆", countDaily);
     await jd_bean_home_module.run();
-    countDaily++;
   },
   1000 * 60 * 60 * 24 // 每天执行一次
 );
 
-setInterval(async function()
+setInterval
+(
+  async function()
   {
-    myLog("一小时一运行 - 东东农场", countMinutes);
+    countMinutes++;    
+    myLog("每 60 分钟运行 - 东东农场", countMinutes);
     await jd_fruit_module.run();
-    countMinutes++;
+    myLog("每 60 分钟运行 - 京喜牧场", countMinutes);
+    await jd_jxmc_module.run();
   },
   1000 * 60 * 60 // 每 60 分钟执行一次
 );
